6c83058a8db5e838d831d253f83a71130651a50d
Aleksei Scrobot <Aleksei_Scrobot@epam.com>
2021-04-20 13:31:56 +0700

    Initial commit

---
+++ src/main/java/com/github/scrobot/ftplistener/FtpListenerApplication.java
@@ -0,0 +1,13 @@
+package com.github.scrobot.ftplistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtpListenerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FtpListenerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/scrobot/ftplistener/FtpListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.scrobot.ftplistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtpListenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
